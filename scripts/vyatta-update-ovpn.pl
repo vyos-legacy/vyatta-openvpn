6fc9d0c1995500077ced43d539386e57ad815a2a
Victoria Efimova <vefimova@mirantis.com>
2013-03-28 20:12:36 +0400

    Bug 8819: Added call of multicast scripts which restores configuration after openvpn daemon restart

--- scripts/vyatta-update-ovpn.pl
+++ scripts/vyatta-update-ovpn.pl
@@ -6,6 +6,7 @@ use Vyatta::OpenVPN::Config;
 
 my $vtun = shift;
 my $statusDir = "/opt/vyatta/etc/openvpn/status";
+my $multicast_script = "/opt/vyatta/sbin/vyatta_multicast_config.pl";
 
 my $config = new Vyatta::OpenVPN::Config;
 my $oconfig = new Vyatta::OpenVPN::Config;
@@ -44,6 +45,11 @@ if ($config->isRestartNeeded($oconfig) && defined($cmd)) {
      if ($? >> 8) {
        $err = 'Failed to start OpenVPN tunnel';
      }
+   if (-x $multicast_script) {
+     if (system("$multicast_script --if_type openvpn --if_name $vtun") != 0) {
+        die "Error during execution of $multicast_script";
+     }
+   }
   }
 }
 my $description = $config->{_description};
